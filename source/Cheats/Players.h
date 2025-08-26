20c646d3fc1063369089aa515cdaa95c072805f7
warwick320 <sihu687@gmail.com>
2025-08-26 22:06:09 +0900

    Refactor player handling and add instance tree saving

--- source/Cheats/Players.h
+++ source/Cheats/Players.h
@@ -4,56 +4,134 @@
 inline RBX::Instance g_LocalPlayer;
 inline RBX::Player g_LocalPlayerInstance;
 inline std::vector<RBX::Player> g_PlayerList;
+inline std::vector<RBX::Player> pf_PlayerPartList;
+inline std::vector<RBX::Player> pf_PlayerPartList2;
+void pfPlayers() {
 
-//void updatePlayers() {
-//	RBX::Instance playerService = dataModel->findFirstChild("Players");
-//
-//	
-//	while (true) {
-//		g_LocalPlayer = comms->read<u64>(playerService.getAddress() + Offsets::LocalPlayer);
-//
-//		for (RBX::Instance& player : playerService.getChildren()) {
-//			if (!player.getAddress()) {
-//				continue;
-//			}
-//			g_PlayerList.push_back(RBX::Player(player.getAddress()));
-//
-//		}
-//		sleep_ms(100);
-//		g_PlayerList.clear();
-//	}
-//}
-void updatePlayers() {
-    RBX::Instance playerService = dataModel->findFirstChild("Players");
+	std::vector<RBX::Instance> all_robox_instance = dataModel->getChildren();
+	printf("Start Found Pf Players\n");
 
-    while (true) {
-		//printf("Updating players...\n");
-        if (playerService.getAddress() == 0){
-			// 플레이어 서비스가 없으면 다시 찾기
-			printf("PlayerService not found, retrying...\n");
-
-			playerService = dataModel->findFirstChild("Players");
-        }
-        
-        g_LocalPlayer = comms->read<u64>(playerService.getAddress() + Offsets::LocalPlayer);
-		if (g_LocalPlayer.getAddress() == 0) {
-			printf("LocalPlayer not found, retrying...\n");
-            playerService = dataModel->findFirstChild("Players");
-			sleep_ms(100);
-			continue;
+	RBX::Instance workspace;
+	for (RBX::Instance& instance : all_robox_instance) {
+		if (instance.getClassName() == "Workspace") {
+			workspace = instance;
+			break;
 		}
-        // g_LocalPlayerInstance도 설정
-        if (g_LocalPlayer.getAddress()) {
-            g_LocalPlayerInstance = RBX::Player(g_LocalPlayer.getAddress());
-        }
+	}
 
-        for (RBX::Instance& player : playerService.getChildern()) {
-            if (!player.getAddress()) {
-                continue;
-            }
-            g_PlayerList.push_back(RBX::Player(player.getAddress()));
-        }
-        sleep_ms(100);
-        g_PlayerList.clear();
-    }
+	if (workspace.getAddress() == 0) {
+		printf("Workspace not found\n");
+		return;
+	}
+
+
+	RBX::Instance players = workspace.findFirstChild("Players");
+	if (players.getAddress() == 0) {
+		printf("Players not found in Workspace\n");
+		return;
+	}
+	RBX::Instance Ava;
+	i32 index = 0;
+	while (true) {
+		std::vector<RBX::Instance> playersChildren = players.getChildren();
+		for (RBX::Instance& child1 : playersChildren) {
+			std::vector<RBX::Instance> folderChild = child1.getChildren();
+			for (RBX::Instance& child : folderChild) {
+				//printf("Checking child: %s, Class: %s\n", child.getName().c_str(), child.getClassName().c_str());
+				if (child.getClassName() == "Model") {
+					std::vector<RBX::Instance> modelChildren = child.getChildren();
+					Ava = child;
+					for (RBX::Instance& part : modelChildren) {
+						if (part.getClassName() == "Part") {
+							RBX::Instance nameTagGui = part.findFirstChild("NameTagGui");
+							if (nameTagGui.getAddress() != 0) {
+								pf_PlayerPartList.push_back(RBX::Player(part.getAddress()));
+								printf("Found player part at : %s, Address: 0x%llx\n", part.getName().c_str(), part.getAddress());
+							}
+						}
+					}
+				}
+
+			}
+
+		}
+		sleep_ms(50);
+		pf_PlayerPartList.clear();
+
+	}
+}
+void updatePlayers() {
+	RBX::Instance playerService = dataModel->findFristChildByClass("Players");
+
+
+	while (true) {
+		if (toggePF) {
+			printf("PlayerService not found\n");
+			pfPlayers();
+		}
+		g_LocalPlayer = comms->read<u64>(playerService.getAddress() + Offsets::LocalPlayer);
+
+		for (RBX::Instance& player : playerService.getChildren()) {
+			if (!player.getAddress()) {
+				continue;
+			}
+			g_PlayerList.push_back(RBX::Player(player.getAddress()));
+
+		}
+		sleep_ms(100);
+		g_PlayerList.clear();
+	}
+}
+
+
+void getAllInstancesRecursive(RBX::Instance& instance, std::vector<RBX::Instance>& result) {
+	result.push_back(instance);
+	std::vector<RBX::Instance> children = instance.getChildren();
+	for (RBX::Instance& child : children) {
+		getAllInstancesRecursive(child, result);
+	}
+}
+
+void getAll() {
+	std::vector<RBX::Instance> all_robox_instance = dataModel->getChildren();
+	std::vector<RBX::Instance> all_instances;
+	for (RBX::Instance& instance : all_robox_instance) {
+		getAllInstancesRecursive(instance, all_instances);
+	}
+}
+#include <fstream>
+#include <functional>
+void saveInstanceTreeToFile(std::vector<RBX::Instance>& instances, std::string& filename) {
+	std::ofstream file(filename);
+	if (!file.is_open()) return;
+
+	std::function<void(RBX::Instance&, int, std::string)> writeTree;
+	writeTree = [&file, &writeTree](RBX::Instance& instance, int depth, std::string path) {
+
+		for (int i = 0; i < depth; ++i) file << "  ";
+
+
+		std::string currentPath = path.empty() ? instance.getClassName() : path + " -> " + instance.getClassName();
+		file << instance.getName() << " [" << instance.getClassName() << "] (Path: " << currentPath << ")\n";
+
+		std::vector<RBX::Instance> children = instance.getChildren();
+		for (size_t i = 0; i < children.size(); ++i) {
+			writeTree(children[i], depth + 1, currentPath);
+		}
+		};
+
+	for (size_t i = 0; i < instances.size(); ++i) {
+		writeTree(instances[i], 0, "");
+	}
+	file.close();
+}
+
+void getAllAndSaveTree() {
+	std::vector<RBX::Instance> all_robox_instance = dataModel->getChildren();
+	std::vector<RBX::Instance> all_instances;
+	for (RBX::Instance& instance : all_robox_instance) {
+		getAllInstancesRecursive(instance, all_instances);
+	}
+	std::string filename = "InstanceTree.txt";
+	saveInstanceTreeToFile(all_robox_instance, filename);
 }
